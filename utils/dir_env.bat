c95dcfb948aaff92d0bf2fe798c0a355b170252e
Steve Sides <stevesides@gmail.com>
2019-05-01 10:51:12 -0500

    added a comment line and made 1 char change.
    but diff (on git-bash) shows 2 lines w/ ^M

--- utils/dir_env.bat
+++ utils/dir_env.bat
@@ -1,5 +1,6 @@
 @echo off
-echo setting DIRCMD: /og /on
+echo Setting DIRCMD: /og /on
+echo Setting PROMPT for 2 lines, too
 set DIRCMD=/og /on
 :: setting PROMPT to 2 lines, with time and dir on top line, 
 :: use $H to erase ".SS"
